2d5e85447b2c8d42302ecd92e099d1a62e72719f
Arina-Naumova1 <areeennna@gmail.com>
2024-10-24 20:32:17 +0300

    Create reg.php

---
+++ reg.php
@@ -0,0 +1,48 @@
+<?php
+if(isset($_POST['login'])) {
+	$errors = array();
+	if(trim($_POST['login']) == '') {
+		$errors[] = "Введите логин!";
+	}
+	if(trim($_POST['email']) == '') {
+		$errors[] = "Введите Email";
+	}
+	if(trim($_POST['name']) == '') {
+		$errors[] = "Введите Имя";
+	}
+	if($_POST['password'] == '') {
+
+		$errors[] = "Введите пароль";
+	}
+	if($_POST['password_2'] != $_POST['password']) {
+		$errors[] = "Повторный пароль введен не верно!";
+    }
+    if (mb_strlen($_POST['name']) < 3 || mb_strlen($_POST['name']) > 50){   
+	    $errors[] = "Недопустимая длина имени";
+    }
+    if (mb_strlen($_POST['password']) < 2 || mb_strlen($_POST['password']) > 8){
+	    $errors[] = "Недопустимая длина пароля (от 2 до 8 символов)";
+    }
+
+$connect = mysqli_connect($dbhost, $dbuser, $dbpassword, $database) or die("Connection Error: " . mysqli_error($connect ));
+ 	{
+
+		$login=mysqli_real_escape_string($connect,trim($_POST['login']));
+		$pass = hash('sha256', trim($_POST['password']));
+		$SQL="SELECT * FROM `users` WHERE `login` = '$login'";// AND `pass` = '$pass'";
+		$check_user = mysqli_query($connect,$SQL) or die("Connection Error: " . mysqli_error($connect));
+		if (mysqli_num_rows($check_user) > 0) {
+			echo "<h2>Такой пользователь уже есть</h2>";
+		} else {
+			$SQL="INSERT INTO `users` (`login`, `pass`) VALUES ( '$login', '$pass');";
+			$insert=mysqli_query($connect,$SQL) or die("Connection Error: " . mysqli_error($connect));
+			if (!$insert) 
+			{
+				$_SESSION["messageSQL"]=mysqli_error($connect);
+			} 
+			else
+			{
+				echo "<h2>Учётная запись создана</h2>";
+			}
+		}
+	}
