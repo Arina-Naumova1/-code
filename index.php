98b87fb56d838b2046a24c730c3bdf895aa3f1da
Яна <1995ivanova@gmail.com>
2024-10-25 16:58:56 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<div id="a_form" title="Вход">
+    <table>
+        <tr>
+            <td><label for="a_name">Логин </label></td>
+            <td><input type='text' id='a_name' value=''></td>
+        </tr>
+        <tr>
+            <td><label for="a_pass">Пароль </label></td>
+            <td><input type='password' id='a_pass' value=''></td>
+        </tr>
+    </table>
+    <div id="a_capcha"></div>
+
+</div>
+<div id="r_form" title="Регистрация">
+    <table>
+        <tr>
+            <td><label for="r_name">Name:</label></td>
+            <td><input type='text' id='r_name' value=''>
+        </tr>
+        <tr>
+            <td><label for="r1_pass">Пароль:</label></td>
+            <td><input type='password' id='r1_pass' value=''>
+        </tr>
+        <tr>
+            <td><label for="r2_pass">Повторите пароль:</label></td>
+            <td><input type='password' id='r2_pass' value=''>
+        </tr>
+        <tr>
+            <td><label for="r_email">Почта:</label></td>
+            <td><input type='email' id='r_email' value=''>
+        </tr>
+    </table>
+    <div id="r_capcha"></div>
+</div>
+
+</div>
