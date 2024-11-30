2cc3a763c3ad3cde247c9738b8e47adbdd072f0f
Яна <1995ivanova@gmail.com>
2024-11-30 18:17:20 +0300

    Add files via upload

--- bd.php
+++ bd.php
@@ -4,9 +4,10 @@ if ($_SERVER["SERVER_NAME"] == '172.19.3.23') {
     $dbuser = 'ivanova.ya.d';
     $dbpassword = '3227';
     $database = 'ivanova.ya.d';
-} else {
+
+  } else {
     $dbhost = 'localhost';
     $dbuser = 'demo';
     $dbpassword = 'demo';
     $database = 'demo';
-}
+  }
