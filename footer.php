02f3c1044b06107f09e0bc4c1649de541740103c
Яна <1995ivanova@gmail.com>
2024-12-11 21:27:02 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="text-center">
+        <div class="container">
+            <p>&copy; 2024 MovieSite. Все права защищены.</p>
+        </div>
+</footer>
--- logout.php
+++ logout.php
@@ -3,4 +3,10 @@
 	unset($_SESSION['session_username']);
 	session_destroy();
 	header("location:login.php");
-	?>
+?>
+<html>
+<div id="welcome">
+<h2>Вы вышли с сайта</h2>
+  <p><a href="login.php">Войти</a> из системы</p>
+</div>
+</html>
